126461c9dfec959cb3eb5bf476a73aed4d452e00
FabienDufoulon <fabien.dufoulon@supelec.fr>
2014-10-21 17:44:04 +0200

    pathFinding ajouté (graphique)
    Ajout d'un fichier et d'une classe pathFinding qui s'occupe du
    traitement de chemin et de l'affichage de la nouvelle matrice

---
+++ Informatique/RechercheChemin/path-testgraphic.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 20 22:50:52 2014
+
+@author: Fabien
+"""
+
+import aStar
+import pathTools
+
+class Pathfinding():
+    def __init__(self, matrix, start, goal):
+        self.matrix = matrix
+        self.start = start
+        self.goal = goal
+
+    def resetPath(self, start, goal): #method that changes start and goal
+        self.start = start
+        self.goal = goal
+        # reset matrix?
+    
+    def findPath(self):
+        a = aStar.AStar(matrix)
+        self.path = a.constructPath(self.start, self.goal)
+        p = pathTools.Path(self.path)
+        self.path = p.path
+        self.highlightPath(4)        
+        
+        p.findShortcut(self.matrix)
+        p.clearPath()
+        self.path = p.path
+        self.highlightPath(5)
+        
+    def highlightPath(self, number):
+        for coord in self.path:
+            self.matrix[coord[0]][coord[1]] = number
+            
+    def printMatrix(self):
+        for line in self.matrix:
+            print line
+            
+
+
+matrix = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,1,1,0,1,1,1,1,1,1,0,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0],
+    [0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+print (3,1)[0]    
+pathF = Pathfinding(matrix, (3,1), (8,12))
+pathF.findPath()
+pathF.printMatrix()
--- README.md
+++
@@ -1,4 +0,0 @@
-RobotsSUMO2017
-==============
-
-Repository for code of Supelec Robotics Team 2017
