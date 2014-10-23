ee51dd156aaa81297d502c51430856ff8d2df876
Askannz <aska990@hotmail.fr>
2014-10-23 23:07:45 +0200

    Commentaires+ajout fichier
    Ajout de commentaires dans le code
    ajout du programme "reconnaissance de couleurs+affinage"

--- Informatique/MachineLearning/calibrationHSV.py
+++ Informatique/MachineLearning/calibrationHSV.py
@@ -1,17 +1,21 @@
+﻿# Même chose que colorDetectImage, mais avec votre webcam au lieu d'une image fixe
+
 import numpy as np
 import cv2
 
 def nothing(x):
     pass
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0) #Ouverture de la caméra
 end = False
 
 cv2.namedWindow('Camera')
 
+#Définition de deux formes géométriques (des croix ici) qui servent aux fonctions erode et dilate
 erodeElement = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
 dilatedElement = cv2.getStructuringElement(cv2.MORPH_CROSS,(7,7))
 
+#Création des trackbars
 Hmin_tb = cv2.createTrackbar('Hmin', 'Camera', 0, 179, nothing)
 Hmax_tb = cv2.createTrackbar('Hmax', 'Camera', 0, 179, nothing)
 Smin_tb = cv2.createTrackbar('Smin', 'Camera', 0, 255, nothing)
@@ -20,12 +24,12 @@ Vmin_tb = cv2.createTrackbar('Vmin', 'Camera', 0, 255, nothing)
 Vmax_tb = cv2.createTrackbar('Vmax', 'Camera', 0, 255, nothing)
 
 while(cap.isOpened() and not end):
-    ret,frame = cap.read()
+    ret,frame = cap.read() #Lecture d'une frame de la caméra (ret est un booléen confirmant que la frame a bien été lue)
 
+    #Récupération des valeurs des trackbars
     Hmin = cv2.getTrackbarPos('Hmin', 'Camera')
     Smin = cv2.getTrackbarPos('Smin', 'Camera')
     Vmin = cv2.getTrackbarPos('Vmin', 'Camera')
-
     Hmax = cv2.getTrackbarPos('Hmax', 'Camera')
     Smax = cv2.getTrackbarPos('Smax', 'Camera')
     Vmax = cv2.getTrackbarPos('Vmax', 'Camera')
@@ -34,10 +38,14 @@ while(cap.isOpened() and not end):
     color_h = np.array([Hmax,Smax,Vmax])
     
     if(ret):
+        #Limage de base est codée en RGB (ou BGR plutôt), on la convertit en HSV
         hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        #On crée un masque à partir des zones de l'image qui sont dans la bonne étendue de couleurs
         mask = cv2.inRange(hsvFrame, color_l, color_h)
+        #On retire de l'image tout ce qui n'est pas de la bonne couleur
         cropped = cv2.bitwise_and(frame, frame, mask = mask)
-        
+
+        #Afffichage du résultat
         cv2.imshow('Camera', cropped)
         
     if(cv2.waitKey(1) & 0xFF == ord('q')):
--- Informatique/MachineLearning/colorDetectImage_enhancing.py
+++ Informatique/MachineLearning/colorDetectImage_enhancing.py
@@ -1,9 +1,7 @@
-﻿# Programme qui ne sélectionne qu'une certaine étendue de couleurs dans l'image
-# Seuls sont affichés les pixels dont les valeurs HSV (Hue Saturation Value)
-# sont comprises entre certaines valeurs, définies par les trackbars
+﻿# Même chose que colorDetectImage, mais avec un affinage de l'image en plus,
+# pour essayer de réduire le bruit notamment
+# (C'est encore foireux...)
 
-# Attention, par défaut, toutes les trackbars sont à zéro, mettez les "max" tout
-# à droite pour afficher l'image complète
 
 import numpy as np
 import cv2
@@ -16,6 +14,10 @@ img = cv2.imread('table_small.JPG') #Ouverture de l'image
 end = False
 
 cv2.namedWindow('Color Detection')
+
+#Définition de deux formes géométriques (des croix ici)
+erodeElement = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+dilatedElement = cv2.getStructuringElement(cv2.MORPH_CROSS,(7,7))
 
 #Création des trackbars
 Hmin_tb = cv2.createTrackbar('Hmin', 'Color Detection', 0, 179, nothing)
@@ -31,8 +33,7 @@ while(not end):
     Hmin = cv2.getTrackbarPos('Hmin', 'Color Detection')
     Smin = cv2.getTrackbarPos('Smin', 'Color Detection')
     Vmin = cv2.getTrackbarPos('Vmin', 'Color Detection')
-
-    Hmax = cv2.getTrackbarPos('Hmax', 'Color Detection')
+    Hmax = cv2.getTrackbarPos('Hmax', 'Color Detection') 
     Smax = cv2.getTrackbarPos('Smax', 'Color Detection')
     Vmax = cv2.getTrackbarPos('Vmax', 'Color Detection')
 
@@ -46,8 +47,20 @@ while(not end):
 
     #On retire de l'image tout ce qui n'est pas de la bonne couleur
     cropped = cv2.bitwise_and(img, img, mask = mask)
+
+    #Amélioration de l'image :
+    #Conversion en niveaux de gris
+    gray = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)
+    #Fonction seuil (conversion en noir/blanc)
+    _,thrsld = cv2.threshold(gray,10,255,cv2.THRESH_BINARY)
+    #Suppression des points blancs isolés
+    eroded = cv2.erode(thrsld,erodeElement)
+    #Suppression des points noirs isolés
+    dilated = cv2.dilate(thrsld,dilatedElement)
+    #Floutage du résultat
+    blurred = cv2.blur(dilated,(3,3))
         
-    cv2.imshow('Color Detection', cropped) #Affichage du résultat
+    cv2.imshow('Color Detection', dilated) #Affichage du résultat
         
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         end = True
