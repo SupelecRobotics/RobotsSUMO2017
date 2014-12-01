69825d214e94c9dc2d947e46adf58cea4b50e84b
AntoineM <aetol@live.fr>
2014-12-02 00:00:21 +0100

    unimportant

--- Informatique/RechercheChemin/path-test.py
+++ Informatique/RechercheChemin/path-test.py
@@ -8,6 +8,7 @@ Created on Mon Oct 20 21:19:10 2014
 import aStar
 import pathTools
 
+"""
 matrix = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
     [0,2,2,2,0,1,1,1,1,1,1,1,1,1,1,0],
@@ -38,3 +39,26 @@ print p.path
 p.clearPath()
 
 print p.path
+"""
+
+a = aStar.AStar(f.forest)
+p = pathTools.Path(a.constructPath(begin, end))
+p.findShortcut(f.forest)
+#p.clearPath()
+
+for i in xrange(0,len(p.path)) :
+    x, y = p.path[i]
+    f.forest[x][y] = 6
+
+for x in xrange(0,side) :
+    str = ""
+    for y in xrange(0,side) :
+        n = f.forest[x][y]
+        if n == 0 :
+            str += "# "
+        elif n == 6 :
+            str += "@ "
+        else :
+            str += "  "
+    print str
+
