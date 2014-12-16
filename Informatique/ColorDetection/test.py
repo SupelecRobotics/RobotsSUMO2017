9fea5355060d5d1b64139cd5a3a159a5514c725b
darkpudding <loucarapuce2685939@hotmail.com>
2014-12-17 00:16:45 +0100

    Ajout test, images
    Ajout test, images, colordetection sur une image.

---
+++ Informatique/ColorDetection/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 16 21:43:54 2014
+
+@author: Darkpudding
+"""
+
+import cv2
+import ColorDetection as cd
+
+cd.ColorDetection.runImage(cv2.imread('test1.jpg'))
