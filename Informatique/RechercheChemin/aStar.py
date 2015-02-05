64d71bf57a80b6c8bfdcc83b467ec484780cb9e9
AntoineM <aetol@live.fr>
2015-02-06 00:01:59 +0100

    Change l'implementation de A* : abandon de la classe 'Cell', stockage des parametres dans plusieurs matrices

--- Informatique/RechercheChemin/aStar.py
+++ Informatique/RechercheChemin/aStar.py
@@ -9,61 +9,61 @@ import math
 import heapq
 import util
 
-class Cell :
-    """ Implements one grid cell, with relevants attributes, for use in the AStar class
-    """
-    count = 0   # number of instances
-    INIT, IN_OPEN_SET, IN_CLOSED_SET, REMOVED = xrange(4)    # constants
-    
-    def __init__(self, x, y) :
-        """ x, y : int, isFree : bool
-        """
-        self.state = Cell.INIT
-        self.x = x
-        self.y = y
-        self.gScore = float('inf')
-        self.fScore = float('inf')
-        self.origin = None
-        self.id = Cell.count    # constant
-        Cell.count += 1
-    
-    def __lt__(self, other) :
-        """ other : Cell
-            returns : bool (compares fScore, then gScore (inverse), then id)
-            implemented for use in a HeapQueue
-        """
-        if self.fScore == other.fScore :
-            if self.gScore == other.gScore :
-                return self.id < other.id
-            else :
-                return self.gScore > other.gScore
-        else :
-            return self.fScore < other.fScore
-    
-    def copy(self) :
-        """ returns : Cell (with identical attributes, except id)
-        """
-        new = Cell(self.x, self.y)
-        new.state = self.state
-        new.gScore = self.gScore
-        new.fScore = self.fScore
-        new.origin = self.origin
-        return new
-    
-    def dist(self, x, y) :
-        """ x, y : float
-            returns : float (euclid distance between self and (x,y))
-        """
-        return util.dist((self.x,self.y), (x,y))
-    
-    def neighbors(self) :
-        """ yields : (int,int) (coordinates of side-adjacent cells)
-        """
-        yield (self.x, self.y + 1)
-        yield (self.x, self.y - 1)
-        yield (self.x + 1, self.y)
-        yield (self.x - 1, self.y)
-    
+#class Cell :
+#    """ Implements one grid cell, with relevants attributes, for use in the AStar class
+#    """
+#    count = 0   # number of instances
+#    INIT, IN_OPEN_SET, IN_CLOSED_SET, REMOVED = xrange(4)    # constants
+#    
+#    def __init__(self, x, y) :
+#        """ x, y : int, isFree : bool
+#        """
+#        self.state = Cell.INIT
+#        self.x = x
+#        self.y = y
+#        self.gScore = float('inf')
+#        self.fScore = float('inf')
+#        self.origin = None
+#        self.id = Cell.count    # constant
+#        Cell.count += 1
+#    
+#    def __lt__(self, other) :
+#        """ other : Cell
+#            returns : bool (compares fScore, then gScore (inverse), then id)
+#            implemented for use in a HeapQueue
+#        """
+#        if self.fScore == other.fScore :
+#            if self.gScore == other.gScore :
+#                return self.id < other.id
+#            else :
+#                return self.gScore > other.gScore
+#        else :
+#            return self.fScore < other.fScore
+#    
+#    def copy(self) :
+#        """ returns : Cell (with identical attributes, except id)
+#        """
+#        new = Cell(self.x, self.y)
+#        new.state = self.state
+#        new.gScore = self.gScore
+#        new.fScore = self.fScore
+#        new.origin = self.origin
+#        return new
+#    
+#    def dist(self, x, y) :
+#        """ x, y : float
+#            returns : float (euclid distance between self and (x,y))
+#        """
+#        return util.dist((self.x,self.y), (x,y))
+#    
+#    def neighbors(self) :
+#        """ yields : (int,int) (coordinates of side-adjacent cells)
+#        """
+#        yield (self.x, self.y + 1)
+#        yield (self.x, self.y - 1)
+#        yield (self.x + 1, self.y)
+#        yield (self.x - 1, self.y)
+#    
 
 class AStar :
     """ Implements the A* algorithm on an unweighed 2D grid with obstacles
@@ -82,33 +82,37 @@ class AStar :
         self.goal = goal[0:2]
         self.goalRadius = goal[2]
         self.pathEnd = None
+        height = len(matrix)
+        width = len(matrix[0])
         self.blockMat = matrix
-        self.cellMat = [ [ None for y in xrange(len(matrix[x])) ] for x in xrange(len(matrix)) ]
+        self.statusMat = [ [ 1 for y in xrange(width) ] for x in xrange(height) ]    # 1 = inexplored, 0 = in open set, -1 = in closed set
+        self.gScoreMat = [ [ float('inf') for y in xrange(width) ] for x in xrange(height) ]
+        self.fScoreMat = [ [ float('inf') for y in xrange(width) ] for x in xrange(height) ]
+        self.prevMat = [ [ None for y in xrange(width) ] for x in xrange(height) ]
         self.openSet = []
         for x in xrange(int(math.floor(self.start[0])), 1+int(math.ceil(self.start[0]))) :
             for y in xrange(int(math.floor(self.start[1])), 1+int(math.ceil(self.start[1]))) :
-                startCell = Cell(x, y)
-                gScore = startCell.dist(self.start[0], self.start[1])
-                startCell.gScore = gScore
-                startCell.fScore = gScore + startCell.dist(self.goal[0], self.goal[1])
-                self.addToOpenSet(startCell)
+                gScore = util.dist(self.start, (x,y))
+                self.gScoreMat[x][y] = gScore
+                self.fScoreMat[x][y] = gScore + self.heuristicEstimate((x,y))
+                self.addToOpenSet(x,y)
     
     def aStar(self) :
         """ runs the A* algorithm
             returns : bool (success)
         """
-        for current in self.lowestCell() :
-            if self.isGoal(current) :    # the algorithm ends if 'goal' is reached
-                self.pathEnd = current.x, current.y
+        for x0, y0 in self.lowestCell() :
+            if self.isGoal((x0,y0)) :    # the algorithm ends if 'goal' is reached
+                self.pathEnd = (x0, y0)
                 return True
-            current.state = Cell.IN_CLOSED_SET
-            for neighbor in self.neighbors(current) :
-                newGScore = current.gScore + current.dist(neighbor.x, neighbor.y)
-                if neighbor.state == Cell.INIT or neighbor.gScore > newGScore :
-                    neighbor.gScore = newGScore
-                    neighbor.fScore = newGScore + self.heuristicEstimate(neighbor)
-                    neighbor.origin = current
-                    self.addToOpenSet(neighbor)
+            self.statusMat[x0][y0] = -1
+            for x1, y1 in self.neighbors(x0, y0) :
+                newGScore = self.gScoreMat[x0][y0] + util.dist((x0,y0), (x1,y1))
+                if self.statusMat[x1][y1] == 1 or self.gScoreMat[x1][y1] > newGScore :
+                    self.gScoreMat[x1][y1] = newGScore
+                    self.fScoreMat[x1][y1] = newGScore + self.heuristicEstimate((x1,y1))
+                    self.prevMat[x1][y1] = (x0,y0)
+                    self.addToOpenSet(x1,y1)
             self.turnCount += 1
         return False
     
@@ -120,26 +124,20 @@ class AStar :
             return None
         else :
             path = [self.pathEnd]
-            current = self.cellMat[self.pathEnd[0]][self.pathEnd[1]]
-            while current.origin != None :
-                current = current.origin
-                coord = (current.x, current.y)
-                path.insert(0,coord)
+            x, y = self.pathEnd
+            while self.prevMat[x][y] != None :
+                x, y = self.prevMat[x][y]
+                path.insert(0, (x,y))
             if self.start != path[0] :
                 path.insert(0, self.start)
-            #if self.goal != self.pathEnd :
-                #path.append(self.goal)
             return path
     
-    def addToOpenSet(self, cell) :
+    def addToOpenSet(self, x, y) :
         """ cell : Cell
         """
-        if cell.state == Cell.IN_OPEN_SET : # if cell is already in openSet : mark it as 'removed' and add a copy
-            cell.state = Cell.REMOVED
-            cell = cell.copy()
-        cell.state = Cell.IN_OPEN_SET
-        self.cellMat[cell.x][cell.y] = cell
-        heapq.heappush(self.openSet, cell)
+        self.statusMat[x][y] = 0
+        item = (self.fScoreMat[x][y], self.cellCount, x, y)
+        heapq.heappush(self.openSet, item)
         self.cellCount += 1
     
     def lowestCell(self) :
@@ -147,30 +145,28 @@ class AStar :
             pop cells from openSet until finding a non-removed one
         """
         while len(self.openSet) > 0 :
-            cell = heapq.heappop(self.openSet)
-            if cell.state == Cell.IN_OPEN_SET :
-                yield cell
+            a, b, x, y = heapq.heappop(self.openSet)
+            if self.statusMat[x][y] == 0 and self.fScoreMat[x][y] == a :
+                yield (x,y)
     
-    def neighbors(self, cell) :
+    def neighbors(self, x0, y0) :
         """ cell : Cell
             yields : Cell (neighbor of 'cell' that are not IN_CLOSED_SET, nor obstacles)
         """
-        for x,y in cell.neighbors() :
+        for x, y in [(x0,y0+1), (x0,y0-1), (x0+1,y0), (x0-1,y0)] :
             if not self.blockMat[x][y] :            # if obstacle : no neighbor
                 pass
             else :
-                if self.cellMat[x][y] == None :     # if void neighbor : create a new cell
-                    yield Cell(x,y)
-                elif self.cellMat[x][y].state == Cell.IN_OPEN_SET : # if neighbor in open set : return it
-                    yield self.cellMat[x][y]
-                else :                              # if neighbor in closed set : don't return it
+                if self.statusMat[x][y] >= 0 :
+                    yield (x,y)
+                else :
                     pass
     
     def heuristicEstimate(self, cell) :
         """ cell : Cell
         """
-        return 1.5 * cell.dist(self.goal[0], self.goal[1])
+        return 1.5 * util.dist(cell, self.goal)
     
     def isGoal(self, cell) :
-        return cell.dist(self.goal[0], self.goal[1]) <= self.goalRadius
+        return util.dist(cell, self.goal) <= self.goalRadius
 
