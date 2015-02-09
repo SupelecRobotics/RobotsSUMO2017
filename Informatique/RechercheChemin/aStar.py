204795bc193dc609543b6c5f957fc43a22d6f703
AntoineM <aetol@live.fr>
2015-02-10 00:06:33 +0100

    Pour test sur pi

--- Informatique/RechercheChemin/aStar.py
+++ Informatique/RechercheChemin/aStar.py
@@ -86,26 +86,26 @@ class AStar :
         height = len(matrix)
         width = len(matrix[0])
         maximum = float('inf')
-        t0 = timeit.default_timer()
+#        t0 = timeit.default_timer()
         self.blockMat = matrix
-        t1 = timeit.default_timer()
-        print 1000 * (t1-t0)
-        t0 = timeit.default_timer()
+#        t1 = timeit.default_timer()
+#        print 1000 * (t1-t0)
+#        t0 = timeit.default_timer()
         self.statusMat = [ [ 1 for y in xrange(width) ] for x in xrange(height) ]    # 1 = inexplored, 0 = in open set, -1 = in closed set
-        t1 = timeit.default_timer()
-        print 1000 * (t1-t0)
-        t0 = timeit.default_timer()
+#        t1 = timeit.default_timer()
+#        print 1000 * (t1-t0)
+#        t0 = timeit.default_timer()
         self.gScoreMat = [ [ maximum for y in xrange(width) ] for x in xrange(height) ]
-        t1 = timeit.default_timer()
-        print 1000 * (t1-t0)
-        t0 = timeit.default_timer()
+#        t1 = timeit.default_timer()
+#        print 1000 * (t1-t0)
+#        t0 = timeit.default_timer()
         self.fScoreMat = [ [ maximum for y in xrange(width) ] for x in xrange(height) ]
-        t1 = timeit.default_timer()
-        print 1000 * (t1-t0)
-        t0 = timeit.default_timer()
+#        t1 = timeit.default_timer()
+#        print 1000 * (t1-t0)
+#        t0 = timeit.default_timer()
         self.prevMat = [ [ None for y in xrange(width) ] for x in xrange(height) ]
-        t1 = timeit.default_timer()
-        print 1000 * (t1-t0)
+#        t1 = timeit.default_timer()
+#        print 1000 * (t1-t0)
         self.openSet = []
         for x in xrange(int(math.floor(self.start[0])), 1+int(math.ceil(self.start[0]))) :
             for y in xrange(int(math.floor(self.start[1])), 1+int(math.ceil(self.start[1]))) :
@@ -113,29 +113,34 @@ class AStar :
                 self.gScoreMat[x][y] = gScore
                 self.fScoreMat[x][y] = gScore + self.heuristicEstimate((x,y))
                 self.addToOpenSet(x,y)
-        self.t0 = timeit.default_timer()
-        self.t = self.t0
     
     def aStar(self) :
         """ runs the A* algorithm
             returns : bool (success)
         """
+        t0 = timeit.default_timer()
         for x0, y0 in self.lowestCell() :
+            print 1000*(timeit.default_timer()-t0)
+            print "top"
+            t0 = timeit.default_timer()
             if self.isGoal((x0,y0)) :    # the algorithm ends if 'goal' is reached
                 self.pathEnd = (x0, y0)
                 return True
+            print 1000*(timeit.default_timer()-t0)
             self.statusMat[x0][y0] = -1
+            print 1000*(timeit.default_timer()-t0)
             for x1, y1 in self.neighbors(x0, y0) :
+                print str(x1) + ", " + str(y1)
+                print 1000*(timeit.default_timer()-t0)
                 newGScore = self.gScoreMat[x0][y0] + util.dist((x0,y0), (x1,y1))
+                print 1000*(timeit.default_timer()-t0)
                 if self.statusMat[x1][y1] == 1 or self.gScoreMat[x1][y1] > newGScore :
                     self.gScoreMat[x1][y1] = newGScore
                     self.fScoreMat[x1][y1] = newGScore + self.heuristicEstimate((x1,y1))
                     self.prevMat[x1][y1] = (x0,y0)
                     self.addToOpenSet(x1,y1)
+                print 1000*(timeit.default_timer()-t0)
             self.turnCount += 1
-            t = timeit.default_timer()
-            print "from astar : " + str(1000*(t - self.t0))+ " -- " + str(1000*(t - self.t))
-            self.t = t
         return False
     
     def buildPath(self) :
@@ -157,30 +162,41 @@ class AStar :
     def addToOpenSet(self, x, y) :
         """ cell : Cell
         """
+        t0 = timeit.default_timer()
         self.statusMat[x][y] = 0
         item = (self.fScoreMat[x][y], self.cellCount, x, y)
         heapq.heappush(self.openSet, item)
         self.cellCount += 1
+        t1 = timeit.default_timer()
+        #print "from astar:addToOPenSet : " + str(1000*(t1-t0))
     
     def lowestCell(self) :
         """ yields : Cell
             pop cells from openSet until finding a non-removed one
         """
+        t0 = timeit.default_timer()
         while len(self.openSet) > 0 :
             a, b, x, y = heapq.heappop(self.openSet)
             if self.statusMat[x][y] == 0 and self.fScoreMat[x][y] == a :
+                t1 = timeit.default_timer()
+                #print "from astar:lowestCell : " + str(1000*(t1-t0))
                 yield (x,y)
+                t0 = timeit.default_timer()
     
     def neighbors(self, x0, y0) :
         """ cell : Cell
             yields : Cell (neighbor of 'cell' that are not IN_CLOSED_SET, nor obstacles)
         """
         for x, y in [(x0,y0+1), (x0,y0-1), (x0+1,y0), (x0-1,y0)] :
+            t0 = timeit.default_timer()
             if not self.blockMat[x][y] :            # if obstacle : no neighbor
                 pass
             else :
                 if self.statusMat[x][y] >= 0 :
+                    t1 = timeit.default_timer()
+                    #print "from astar:neigbors : " + str(1000*(t1-t0))
                     yield (x,y)
+                    t0 = timeit.default_timer()
                 else :
                     pass
     
