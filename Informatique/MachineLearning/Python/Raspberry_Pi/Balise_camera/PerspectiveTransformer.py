eeaf69880f8edd77cc9ddf22de194408193e6416
Askannz <aska990@hotmail.fr>
2015-02-11 21:46:23 +0100

    Programme de calibrage transformation perspective

--- Informatique/MachineLearning/Python/Raspberry_Pi/Balise_camera/PerspectiveTransformer.py
+++ Informatique/MachineLearning/Python/Raspberry_Pi/Balise_camera/PerspectiveTransformer.py
@@ -5,12 +5,16 @@ class PerspectiveTransformer:
 
     # Classe qui gère la transformation entre les coordonnées d'un point sur une frame
     # et ses coordonnées réelles sur la table.
-    # Se calibre en utilisant des points de référence.
+    # Charge ses paramètres depuis le fichier PerspectiveTransformer.dat, lui-même créé par le programme perspectiveCalibration.py
+    
 
-    def calibrate(self):
-        frameRefPoints = np.float32([(55, 323),(17,370),(443,319),(139,334)]).reshape(-1,1,2)  #Temporaire
-        tableRefPoints = np.float32([(0,0),(33,233),(334,79),(84,77)]).reshape(-1,1,2) #Temporaire
-        self.M,_ = cv2.findHomography(frameRefPoints, tableRefPoints, cv2.RANSAC,5.0)
+    def loadParamFromFile(self):
+        with open('PerspectiveTransformer.dat', 'r') as file:
+            depickler = pickle.Unpickler(file)
+            data = depickler.load()
+            frameRefPoints = np.float32(data[0])
+            tableRefPoints = np.float32(data[1])
+            self.M,_ = cv2.findHomography(frameRefPoints, tableRefPoints, cv2.RANSAC,5.0)
 
     def transform(self, srcPoints):
         return cv2.perspectiveTransform(srcPoints.reshape(-1,1,2),self.M)
--- Informatique/Voitures/FrameProcessor.py
+++ Informatique/Voitures/FrameProcessor.py
@@ -1,18 +1,15 @@
 import cv2
-import pickle
-import numpy as np
+import numpy
 
+cap = cv2.VideoCapture(0)
 
 class FrameProcessor:
 
     def __init__(self):
-        self.cross = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-        with open('param.dat', 'r') as file:
-            depickler = pickle.Unpickler(file)
-            self.data = depickler.load()
+        
 
     def isMarkVisible(self, frame):
-        cropped = cv2.inRange(frame, self.data["colorMin"], self.data["colorMax"])
+        cropped = cv2.inRange(frame, self.colorMin, self.colorMax)
         eroded = cv2.erode(cropped, self.cross)
         contours,_ = cv2.findContours(eroded,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -20,7 +17,7 @@ class FrameProcessor:
 
         for cnt in contours:
             x,y,w,h = cv2.boundingRect(cnt)
-            if(w > self.data["Wmin"] and h > self.data["Hmin"]  and w < self.data["Wmax"]  and h < self.data["Hmax"] ):
+            if(w > self.wMin and h > self.hMin and w < self.wMax and h < self.hMax):
                 foundMark = True
                 break
             
