5a9fe4e3359c3eea8c2fffa7061680f81d61e062
Fabien Dufoulon <pi@raspberrypi.(none)>
2015-02-16 15:16:15 +0000

    Communication Rpi Arduino par bluetooth

--- Informatique/CommunicationSerial/testCom2.py
+++ Informatique/CommunicationSerial/testCom2.py
@@ -16,10 +16,10 @@ import time # pour le délai d'attente entre les messages
  
 ser = serial.Serial('/dev/ttyACM0', 9600)
 time.sleep(4) #on attend un peu, pour que l'Arduino soit prêt.
-d = 400             #distance : 2 byte max
+d = 10             #distance : 2 byte max
 d1 = d >> 8
 d2 = d - (d1 << 8)
-theta = 300         #theta : 2 byte max
+theta = 0         #theta : 2 byte max
 t1 = theta >> 8
 t2 = theta - (t1 << 8)
 satVitesse = 120    #saturation vitesse : 1 byte max
