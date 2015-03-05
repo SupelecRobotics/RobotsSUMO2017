38d493148a1eeda49babd4aa6708cefb8f80e7a6
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-03-06 00:19:53 +0100

    Com

---
+++ Informatique/CommunicationSerial/ScriptsRobot/scriptAddModule.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 05 23:16:19 2015
+
+@author: Fabien
+"""
+
+import sys
+sys.path.append("C:\\Users\\Fabien\\MyDocuments\\Github\\RobotsSUMO2017\\Informatique")
+print sys.path
+
+from Informatique import CommunicationSerial
---
+++ Informatique/CommunicationSerial/script1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 05 22:28:05 2015
+
+@author: Fabien
+"""
+import CommunicationSerial
+
+com = CommunicationSerial('/dev/ttyACM0','/dev/ttyACM0')
+
+com.envoiMoteurCapteur(0,100,0)
+com.envoiMoteurCapteur(0,0,450)
+com.envoiMoteurCapteur(0,100,0)
+com.envoiMoteurCapteur(0,0,450)
+
+com.envoiMoteurCapteur(0,0,-450)
+com.envoiMoteurCapteur(0,-100,0)
+com.envoiMoteurCapteur(0,0,-450)
+com.envoiMoteurCapteur(0,-100,0)
