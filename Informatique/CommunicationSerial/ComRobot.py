3ba4c2098dc90e99ad96e6e1cc992107c78238f7
Fabien Dufoulon <pi@raspberrypi.(none)>
2015-03-05 21:05:18 +0000

    Communication

--- Informatique/CommunicationSerial/ComRobot.py
+++ Informatique/CommunicationSerial/ComRobot.py
@@ -8,9 +8,10 @@ Created on Thu Mar 05 21:36:47 2015
 import serial # bibliothèque permettant la communication série
 import time # pour le délai d'attente entre les messages
  
-serControleRobot = serial.Serial('/dev/ttyACM0', 9600)
+serMoteurCapteur = serial.Serial('/dev/ttyACM0', 9600)
+time.sleep(3)
 
-def envoi(commande, d, theta):
+def envoi(commande, d=0, theta=0):
     if (d<0):
        d = d + 32768
     d1 = d >> 8
@@ -22,11 +23,17 @@ def envoi(commande, d, theta):
     satVitesse = 200    #saturation vitesse : 1 byte max
     
     inputByteString = chr(commande) + chr(d1) + chr(d2) + chr(t1) + chr(t2) + chr(satVitesse)
-    serControleRobot.write(inputByteString)
+    print serMoteurCapteur.write(inputByteString)
     print("Envoi Byte String")
-    print(serControleRobot.readline())
+    print(serMoteurCapteur.readline())
     time.sleep(0.5)
     
 #envoi(1) #stop
 #envoi(2) #infos
-envoi(0, 10, 0)
+#envoi(0, 100, 0)
+envoi(0, 0, 300)
+#envoi(0, 100, 0)
+#envoi(0, 0, 300)
+#envoi(0 , -100, 0)
+envoi(0, 0, -300)
+envoi(1)
