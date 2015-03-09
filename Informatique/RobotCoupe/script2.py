3e4a610b29e7f5a3e903823712ba5753be0aaf89
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-03-09 20:40:45 +0100

    Robot

--- Informatique/RobotCoupe/script2.py
+++ Informatique/RobotCoupe/script2.py
@@ -1,16 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 06 00:46:02 2015
-
-@author: Fabien
-"""
-
 import time
-from ..communication import CommunicationSerial as com
+from CommunicationSerial import CommunicationSerial as com
 
-#run python -m Robot.scripts.script2 in /Informatique
-
-com = com.CommunicationSerial('/dev/ttyACM0','/dev/ttyACM0')
+com = com('/dev/ttyACM0','/dev/ttyACM0')
 
 com.envoiMoteurCapteur(100,0)
 time.sleep(2)
