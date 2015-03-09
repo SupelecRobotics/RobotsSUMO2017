0dbac3bdc423dca6104b2b930dcdec898ac59bcd
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-03-09 20:36:21 +0100

    Gros Commit pour le robot

--- Informatique/CommunicationSerial/__init__.py
+++
@@ -1,1 +0,0 @@
-from CommunicationSerial import CommunicationSerial
---
+++ Informatique/Robot/Script.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 07 15:22:16 2015
+
+@author: Fabien
+"""
+
+#import Communication.CommunicationSerial
+import scripts.script1
