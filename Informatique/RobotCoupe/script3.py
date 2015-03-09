a5c5ab5d284b4721c47845289e6d49a4665a3629
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-03-09 21:27:37 +0100

    Script3

---
+++ Informatique/RobotCoupe/script3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from CommunicationSerial import CommunicationSerial as com
+
+com = com('/dev/ttyACM0','/dev/ttyACM0')
+
+com.envoiMoteurCapteur(200,0)
+#com.envoiMoteurCapteur(0,900)
+#com.envoiMoteurCapteur(100,0)
+#com.envoiMoteurCapteur(0,200)
+
+#com.envoiMoteurCapteur(0,-900)
+#com.envoiMoteurCapteur(-200,0)
+#com.envoiMoteurCapteur(200,600)
+#com.envoiMoteurCapteur(0,-200)
+#com.envoiMoteurCapteur(-100,0)
