c949cb9e22f59b70cab53a59402ab2392ece3fc4
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-03-17 01:04:06 +0100

    Script

--- Informatique/RobotCoupe/script2.py
+++ Informatique/RobotCoupe/script2.py
@@ -4,8 +4,15 @@ from CommunicationSerial import CommunicationSerial as com
 
 com = com('/dev/ttyACM0','/dev/ttyACM0')
 
-com.envoiMoteurCapteur(-500,0)
+com.envoiMoteurCapteur(-50,0)
 com.envoiMoteurCapteur(0,0)
 time.sleep(2)
 returned = com.getInfos()
 print returned
+print returned[:2]
+print returned[2:4]
+print returned[4:6]
+print returned[6:7]
+print returned[7:8]
+print returned[8:9]
+print returned[9:10]
