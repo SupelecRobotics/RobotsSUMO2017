9fc9dc8a9262c69f14cd9fb9ff6c77b96598e28f
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-03-30 22:20:46 +0200

    Robot

--- Informatique/RobotCoupe/script7.py
+++ Informatique/RobotCoupe/script7.py
@@ -11,7 +11,7 @@ import time
 
 robot = Robot('/dev/ttyACM0','/dev/ttyACM0')
 robot.printPosition()
-robot.allerA((1700,1000))
+robot.allerA((1500,1200))
 robot.printPosition()
 time.sleep(2)
 robot.printPosition()
