944372ccbce0e75b932bcdf987e8d31b4a9f9233
Guriido <christian.hamelain@laposte.net>
2015-03-30 23:07:07 +0200

    Ajout d'une fonction de recherche de trajectoire avec angle
    méthode comingOut dans le fichier trajectoire.py

--- Informatique/RobotCoupe/trajectoire.py
+++ Informatique/RobotCoupe/trajectoire.py
@@ -54,7 +54,43 @@ class Trajectoire :
         
         return way
 
-    
+    def comingOut(self, point, angleArrivee)   :
+
+        pathMan = PathManager(robomoviesForest.getForest())
+        pathMan.setThreshold(4)
+        (x, y) = point
+        (x, y) = (y/self.facteurDistance, x/self.facteurDistance)
+        pathMan.findPath(self.position[0],(x, y, 0))
+
+        path = pathMan.path
+        
+        coor0 = self.position[0]
+        angle0 = self.position[1]
+        orientation = self.position[2]
+        way = []
+
+              
+        for coor in path[1:] :
+            (x0, y0) = coor0
+            (x, y) = coor
+            distance = dist(coor0, coor)
+            ang = - angle0 + angle((1, 0), (x - x0, y - y0))
+            ang = (ang + math.pi) % (2*math.pi)  - math.pi     # ang dans [-180, 180]
+            way.append((distance*self.facteurDistance, ang*180/math.pi*self.facteurDegre, orientation)) 
+            angle0 = ang
+            coor0 = coor
+
+        ang = - angle0 + angleArrivee
+        ang = (ang + math.pi) % (2*math.pi)  - math.pi     # ang dans [-180, 180]
+        way.append((0, ang*180/math.pi*self.facteurDegre, orientation)) 
+        
+        self.currentWay = way
+
+
+        return way
+
+
+        
 
     def ordersTo(self, point) :
 
