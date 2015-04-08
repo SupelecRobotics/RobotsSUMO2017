741d158c2119d6db0c078f4c375bcee07dd464e9
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-04-08 23:07:53 +0200

    Robot

--- RobotCoupe/Robot.py
+++ RobotCoupe/Robot.py
@@ -62,8 +62,9 @@ class Robot :
             print (distance, angle)
             coor = (self.x, self.y)
             self.com.envoiMoteurCapteur(0,int(angle))
+            time.sleep(1)
             self.com.envoiMoteurCapteur(int(distance),0) #envoi d'entiers
-            time.sleep(2)
+            time.sleep(1)
             
     def orderToPoint(self, point):
         (x0, y0) = (self.x,self.y)
