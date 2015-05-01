41177d565f4a1954fdbe3a4a0964f968501fb7bf
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-05-01 13:57:58 +0200

    Scripts pour vérifier les distances et positions

--- RobotCoupe/script4.py
+++ RobotCoupe/script4.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 import time
-from CommunicationSerial import CommunicationSerial as com
 
-com = com('/dev/ttyACM0','/dev/ttyACM0')
+from Robot import Robot
 
-com.envoiMoteurCapteur(20,0)
-time.sleep(2)
-returned = com.getInfos()
-print returned
-time.sleep(2)
-returned = com.getInfos()
-print returned
+robot = Robot('/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2')
+
+
+while (True):
+    robot.printPosition()
+#    d = raw_input('Enter a distance: ')
+#    theta = raw_input('Enter an angle: ')
+#    robot.bouge(0,int(theta))
+#    robot.bouge(int(d),0)
+    d = raw_input('Enter d : ')
+    theta = raw_input('Enter theta : ')
+    robot.bouge((int(d),int(theta)))
+    time.sleep(1)
