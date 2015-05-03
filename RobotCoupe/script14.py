bdb04d426507cb16d5859ff52f0b439fe33bf6e6
Guriido <christian.hamelain@laposte.net>
2015-05-03 21:05:15 +0200

    modif sur changeAlpha

--- RobotCoupe/script14.py
+++ RobotCoupe/script14.py
@@ -4,17 +4,19 @@
 
 from Robot import Robot
 from calculAngle import donneAlpha
+import time
 
 robot = Robot('/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2')
-robot.printPosition()
-orientationInitiale = True
-sens = True
-l = raw_input('Enter l : ')
-d1 = 100
-d2 = 300
-gobelet = True
-theta = donneAlpha(orientationInitiale, sens, l, d1, d2, gobelet)
-robot.bouge(int(l),int(theta))
+while(True)	:
+	robot.printPosition()
+	orientationInitiale = True
+	sens = True
+	l = raw_input('Enter l : ')
+	d1 = 100
+	d2 = 300
+	gobelet = True
+	theta = donneAlpha(orientationInitiale, sens, l, d1, d2, gobelet)
+	#robot.bouge(int(l),int(theta))
 
-time.sleep(2)
+	time.sleep(2)
 robot.printPosition()
