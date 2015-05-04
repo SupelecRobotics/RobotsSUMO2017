2ee755f29124f0d8938cc14129da652f04e81f29
Guriido <christian.hamelain@laposte.net>
2015-05-04 22:57:02 +0200

    Robot

--- RobotCoupe/script15.py
+++ RobotCoupe/script15.py
@@ -24,7 +24,7 @@ theta = donneAlpha(orientationInitiale, bool(sens), int(l), d1, d2, gobelet)
 profSpot = 7
 L = donneL(theta, int(l), profSpot)
 L = L * 10
-theta = (int)robot.theta + theta * 10
+theta = int(robot.theta) + theta * 10
 
 print theta
 robot.bouge(0,int(theta))
