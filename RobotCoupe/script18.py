bf7ec74dc2ad1cd73fb871d9a4dc747d8ffb2e16
Guriido <christian.hamelain@laposte.net>
2015-05-05 23:47:59 +0200

    Robot

--- RobotCoupe/script18.py
+++ RobotCoupe/script18.py
@@ -21,108 +21,108 @@ print 'post pouet'
 # robot.bouge(0, -900)
 
 
-robot.updatePosition()
+# robot.updatePosition()
 
-(x0, y0) = (robot.x,robot.y)
-(x, y) = (250, 250)
+# (x0, y0) = (robot.x,robot.y)
+# (x, y) = (250, 250)
 
 
-delta = - robot.theta + angle((1, 0), (x - x0, y - y0))*1800/math.pi
+# delta = - robot.theta + angle((1, 0), (x - x0, y - y0))*1800/math.pi
+
+# # robot.bouge(0, int(delta))
+# # robot.updatePosition()
+
+# orientationInitiale = True
+# sens = True
+# robot.printPosition()
+# l = int(dist((robot.x, robot.y), (250, 250))) / 10
+# d1 = 10
+# d2 = 8.5
+# gobelet = True
+# theta = donneAlpha(orientationInitiale, bool(sens), int(l), d1, d2, gobelet)
+# print theta
+# profSpot = 7
+# L = donneL(theta, int(l), profSpot)
+# L = L * 10 + 60
+# theta =  theta * 10
+
+
+# robot.bouge(0,int(theta))
+# time.sleep(1)
+# robot.bouge(int(L),0)
+# time.sleep(0.5)
+# robot.com.appelMonteeActionneurGobeletDevant()
+# time.sleep(1)
+# robot.updatePosition()
+
+# #1er plot
+
+# (x0, y0) = (robot.x,robot.y)
+# (x, y) = (90, 250)
+
+
+# delta = - robot.theta + angle((1, 0), (x - x0, y - y0))*1800/math.pi
 
 # robot.bouge(0, int(delta))
 # robot.updatePosition()
 
-orientationInitiale = True
-sens = True
-robot.printPosition()
-l = int(dist((robot.x, robot.y), (250, 250))) / 10
-d1 = 10
-d2 = 8.5
-gobelet = True
-theta = donneAlpha(orientationInitiale, bool(sens), int(l), d1, d2, gobelet)
-print theta
-profSpot = 7
-L = donneL(theta, int(l), profSpot)
-L = L * 10 + 60
-theta =  theta * 10
+# orientationInitiale = True
+# sens = True
+# robot.printPosition()
+# l = int(dist((robot.x, robot.y), (90, 250))) / 10
+# d1 = 10
+# d2 = 8.5
+# gobelet = False
+# theta = donneAlpha(orientationInitiale, bool(sens), int(l), d1, d2, gobelet)
+# print theta
+# profSpot = 7
+# L = donneL(theta, int(l), profSpot)
+# L = L * 10 + 60
+# theta =  theta * 10
 
 
-robot.bouge(0,int(theta))
-time.sleep(1)
-robot.bouge(int(L),0)
-time.sleep(0.5)
-robot.com.appelMonteeActionneurGobeletDevant()
-time.sleep(1)
-robot.updatePosition()
+# robot.bouge(0,int(theta))
+# time.sleep(1)
+# robot.bouge(int(L),0)
+# time.sleep(3)
 
-#1er plot
+# robot.updatePosition()
 
-(x0, y0) = (robot.x,robot.y)
-(x, y) = (90, 250)
+# #plot 2
 
 
-delta = - robot.theta + angle((1, 0), (x - x0, y - y0))*1800/math.pi
-
-robot.bouge(0, int(delta))
-robot.updatePosition()
-
-orientationInitiale = True
-sens = True
-robot.printPosition()
-l = int(dist((robot.x, robot.y), (90, 250))) / 10
-d1 = 10
-d2 = 8.5
-gobelet = False
-theta = donneAlpha(orientationInitiale, bool(sens), int(l), d1, d2, gobelet)
-print theta
-profSpot = 7
-L = donneL(theta, int(l), profSpot)
-L = L * 10 + 60
-theta =  theta * 10
+# (x0, y0) = (robot.x,robot.y)
+# (x, y) = (90, 150)
 
 
-robot.bouge(0,int(theta))
-time.sleep(1)
-robot.bouge(int(L),0)
-time.sleep(3)
+# delta = - robot.theta + angle((1, 0), (x - x0, y - y0))*1800/math.pi
 
-robot.updatePosition()
+# robot.bouge(0, int(delta))
+# robot.updatePosition()
 
-#plot 2
-
-
-(x0, y0) = (robot.x,robot.y)
-(x, y) = (90, 150)
-
-
-delta = - robot.theta + angle((1, 0), (x - x0, y - y0))*1800/math.pi
-
-robot.bouge(0, int(delta))
-robot.updatePosition()
-
-orientationInitiale = True
-sens = True
-robot.printPosition()
-l = int(dist((robot.x, robot.y), (90, 150))) / 10
-d1 = 10
-d2 = 8.5
-gobelet = False
-theta = donneAlpha(orientationInitiale, bool(sens), int(l), d1, d2, gobelet)
-print theta
-profSpot = 7
-L = donneL(theta, int(l), profSpot)
-L = L * 10 + 60
-theta =  theta * 10
+# orientationInitiale = True
+# sens = True
+# robot.printPosition()
+# l = int(dist((robot.x, robot.y), (90, 150))) / 10
+# d1 = 10
+# d2 = 8.5
+# gobelet = False
+# theta = donneAlpha(orientationInitiale, bool(sens), int(l), d1, d2, gobelet)
+# print theta
+# profSpot = 7
+# L = donneL(theta, int(l), profSpot)
+# L = L * 10 + 60
+# theta =  theta * 10
 
 
-robot.bouge(0,int(theta))
-time.sleep(1)
-robot.bouge(int(L),0)
-time.sleep(3)
+# robot.bouge(0,int(theta))
+# time.sleep(1)
+# robot.bouge(int(L),0)
+# time.sleep(3)
 
-robot.bouge(0, -500)
-robot.bouge(-500, 0)
+# robot.bouge(0, -500)
+# robot.bouge(-500, 0)
 
 
-robot.com.appelDescenteActionneurGobeletDevant()
+# robot.com.appelDescenteActionneurGobeletDevant()
 
