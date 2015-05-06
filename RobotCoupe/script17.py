2d715f09146ce0556a1639ce3dd4abfbf29fabe2
Guriido <christian.hamelain@laposte.net>
2015-05-06 22:21:06 +0200

    Robot

--- RobotCoupe/script17.py
+++ RobotCoupe/script17.py
@@ -10,7 +10,7 @@ import time
 robot = Robot('/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2')
 
 print 'pré pouet'
-robot.allerAangle(( 910, 850), 900)
+robot.allerA(( 910, 850))
 print 'pouet'
 time.sleep(1)
 #robot.bouge(100, 0)
