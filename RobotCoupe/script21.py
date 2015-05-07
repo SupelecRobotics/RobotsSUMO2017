8c21409076b365939b34b07df44fda1aa19dbc52
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-05-08 00:51:12 +0200

    nouveau script

--- RobotCoupe/script21.py
+++ RobotCoupe/script21.py
@@ -6,23 +6,27 @@ import time
 
 robot = Robot('/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2')
 
-#robot.bouge(200, 0)
-print 'pré pouet'
 robot.allerA(( 910, 850))
-print 'pouet'
-time.sleep(1)
-#robot.bouge(100, 0)
-print 'post pouet'
-
-# robot.bouge(0, 900)
-# robot.bouge(0, -900)
-
-
-
 robot.goToCylindreLocal((910, 1170), True)
 
 time.sleep(1)
 
+robot.allerA(( 450, 450))
+robot.goToGobeletLocal((250, 250), True)
+robot.goToCylindreLocal((90, 250), True)
+robot.goToCylindreLocal((90, 150), True)
 
-robot.com.appelDescenteActionneurGobeletDevant()
+
+robot.allerA(( 700, 950))
+robot.goToCylindreLocal((870, 645), False)
+#robot.allerA((900, 520))
+robot.goToCylindreLocal((1100, 230), False)
+robot.goToCylindreLocal((1300, 600), False)
+
+robot.allerA((1250, 400))
+robot.goToGobeletLocal((1500, 350), False)
+
+
+robot.allerAangle((600, 1000), 0)
+robot.allerA((250, 1000))
 
