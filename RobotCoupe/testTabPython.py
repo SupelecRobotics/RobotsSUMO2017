b9b58b043524375d18bd6cea7d476517dbda3b3a
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-05-07 13:52:05 +0200

    Revert "Robot"
    This reverts commit 9523f149477d7ddab0a347c415b875e94e126338.

--- RobotCoupe/testTabPython.py
+++ RobotCoupe/testTabPython.py
@@ -7,29 +7,29 @@ robot = Robot('/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2')
 
 
 while (True):
-    robot.printPosition()
-    #    d = raw_input('Enter a distance: ')
-    #    theta = raw_input('Enter an angle: ')
-    #    robot.bouge(0,int(theta))
-    #    robot.bouge(int(d),0)
-    x = raw_input('Enter x : ')
-    y = raw_input('Enter y : ')
-    theta = raw_input('Enter theta : ')
-    robot.allerAangle((int(x),int(y)), int(theta))
-    time.sleep(1)
+	robot.printPosition()
+	#    d = raw_input('Enter a distance: ')
+	#    theta = raw_input('Enter an angle: ')
+	#    robot.bouge(0,int(theta))
+	#    robot.bouge(int(d),0)
+	x = raw_input('Enter x : ')
+	y = raw_input('Enter y : ')
+	theta = raw_input('Enter theta : ')
+	robot.allerAangle((int(x),int(y)), int(theta))
+	time.sleep(1)
 
 while(True):
-    print 'pouet'
+	print 'pouet'
 
-    print 'pouet'
+	print 'pouet'
 
-    for i in range(0, 1):
-        pouet = 20
+	for i in range(0, 1):
+		pouet = 20
 
-    if(True):
-        print 'true'
-    else :
-        pouet = 1
+	if(True):
+		print 'true'
+	else :
+		pouet = 1
 
 
 
