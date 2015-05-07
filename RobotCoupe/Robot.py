dc4afd1f43aae5374542ac78be30956a5191d94a
Guriido <christian.hamelain@laposte.net>
2015-05-07 17:33:39 +0200

    Robot

--- RobotCoupe/Robot.py
+++ RobotCoupe/Robot.py
@@ -173,6 +173,7 @@ class Robot :
         theta =  theta * 10 + ang
         if(not sens):
             theta += 1800
+            theta = int(theta)%3600
             L = -L
         self.bouge(0,int(theta))
         time.sleep(1)
