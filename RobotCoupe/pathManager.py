cf51675715ea14d54dcf505bcf5ae7c16f2c6fae
Guriido <christian.hamelain@laposte.net>
2015-05-10 20:02:24 +0200

    PathManager

--- RobotCoupe/pathManager.py
+++ RobotCoupe/pathManager.py
@@ -29,7 +29,8 @@ class PathManager :
             constructs thresholdMap from baseMap : 
             values between 0 and -threshold are obstacles (False), other values are free spaces (True)
         """
-        self.thresholdMap = [ [ (self.baseMap[x][y] > 0 or self.baseMap[x][y] < -threshold) for y in xrange(len(self.baseMap[x])) ] for x in xrange(len(self.baseMap)) ]
+        forest = self.baseMap.getForest()
+        self.thresholdMap = [ [ (forest[x][y] > 0 or forest[x][y] < -threshold) for y in xrange(len(forest[x])) ] for x in xrange(len(forest)) ]
     
     def findPath(self, start, goal) :
         """ start : (float,float), goal : (float,float,float) or (float,float)
