7fd80a0e740c0d34777224b85fdfea87ac6fb2f9
Guriido <christian.hamelain@laposte.net>
2015-05-11 22:18:46 +0200

    script25

--- RobotCoupe/script25.py
+++ RobotCoupe/script25.py
@@ -8,75 +8,60 @@ robot = Robot('/dev/ttyACM0','/dev/ttyACM1','/dev/ttyACM2')
 
 # les deux cylindres en haut (P12 et P13)
 
-robot.allerA((730, 1580))
-robot.goToCylindreLocal((850, 1800), True)
-robot.goToCylindreLocal((850, 1900), True)
+robot.allerA((1270, 1580))
+robot.goToCylindreLocal((1150, 1800), False)
+robot.goToCylindreLocal((1150, 1900), False)
 
 # Gobelet G2 + dépose dans la zone de départ
 
-robot.allerA((690, 1050))
+robot.allerA((1310, 1050))
 
-robot.goToCylindreLocal((910, 1170), False)
+robot.goToCylindreLocal((1090, 1170), True)
 
-robot.bouge(0, 900)
 
-# robot.allerAangle((600, 1000), 1800)
-# robot.allerA((250, 1000))
+# Gobelet G + Cylindres 164 et P15
 
-# robot.com.appelDescenteActionneurGobeletDerriere()
-
-# Gobelet G1 + Cylindres P4 et P5
-
-robot.allerA(( 450, 450))
-robot.goToGobeletLocal((250, 250), True)
+robot.allerA(( 1550, 450))
+robot.goToGobeletLocal((1750, 250), False)
 
 #1er plot
-robot.goToCylindreLocal((90, 250), True)
+robot.goToCylindreLocal((1910, 250), False)
 
 #plot 2
-robot.goToCylindreLocal((90, 150), True)
-
-
-
-#3 cylindres + gobelet
-robot.allerA((700, 950))
-robot.goToCylindreLocal((870, 645), False)
-robot.allerA((900, 520))
-robot.goToCylindreLocal((1100, 230), False)
-robot.goToCylindreLocal((1300, 600), False)
-
-robot.allerA((1250, 400))
-robot.goToGobeletLocal((1500, 350), False)
-
-
-#gobelet
-
-robot.allerA((910, 850))
-robot.goToGobeletLocal((910, 1170), True)
-
-#poser gobelet + 3 cylindres + balle
-robot.allerAangle((600, 1000), 0)
-robot.allerA((450, 1000))
-
-#2 cylindres + gobelet
-
-robot.allerA((450, 450))
-robot.goToGobeletLocal((250, 250), True)
-robot.goToCylindreLocal((90, 250), True)
-robot.goToCylindreLocal((90, 150), True)
+robot.goToCylindreLocal((1910, 150), False)
 
 #claps
-robot.allerAangle((int(250),int(230)),-900)
-robot.com.appelMonteeActionneurGobeletDevant()
-robot.com.appelDescenteClapDroit()
-robot.allerAangle((int(250),int(230)), 0)
-robot.com.appelMonteeClapDroit()
+robot.allerAangle((int(1750),int(230)),900)
+robot.com.appelMonteeActionneurGobeletDerriere()
+robot.com.appelDescenteClapGauche()
+robot.allerAangle((int(1750),int(230)), 1800)
+robot.com.appelMonteeClapGauche()
 
-robot.allerAangle((int(700),int(230)), 0)
-robot.com.appelDescenteClapDroit()
+robot.allerAangle((int(1300),int(230)), 1800)
+robot.com.appelDescenteClapGauche()
 #robot.allerAangle((int(900),int(230)), 0)
-robot.bouge(int(200),0)
-robot.com.appelMonteeClapDroit()
+robot.bouge(int(1800),1800)
+robot.com.appelMonteeClapGauche()
+
+
+# 2 cylindres P7 P8 
+robot.goToCylindreLocal((900, 230), True)
+robot.goToCylindreLocal((700, 600), True)
+
+# dépose des 4 cylindres
+robot.allerA((700, 310))
+time.sleep(1)
+
+# cylindre P6
+robot.allerA((960, 950))
+robot.goToCylindreLocal((1130, 645), True)
+
+
+#poser gobelet + 3 cylindres + balle
+robot.allerAangle((1400, 1000), 1800)
+robot.allerA((1550, 1000))
+
+robot.com.appelDescenteActionneurGobeletDevant()
 
 
 
