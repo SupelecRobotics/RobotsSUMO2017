7179be7fd6e9e4a95534e53a155f859e68db96e9
Guriido <christian.hamelain@laposte.net>
2015-05-11 20:27:09 +0200

    script24

--- RobotCoupe/script24.py
+++ RobotCoupe/script24.py
@@ -60,10 +60,10 @@ robot.allerA((450, 1000))
 
 #2 cylindres + gobelet
 
-#robot.allerA((450, 450))
-#robot.goToGobeletLocal((250, 250), True)
-#robot.goToCylindreLocal((90, 250), True)
-#robot.goToCylindreLocal((90, 150), True)
+robot.allerA((450, 450))
+robot.goToGobeletLocal((250, 250), True)
+robot.goToCylindreLocal((90, 250), True)
+robot.goToCylindreLocal((90, 150), True)
 
 #claps
 robot.allerAangle((int(250),int(230)),-900)
@@ -78,20 +78,22 @@ robot.com.appelDescenteClapDroit()
 robot.bouge(int(200),0)
 robot.com.appelMonteeClapDroit()
 
-#chercher cylindres près des distributeurs?? (temps tros short)
 
-#poser 2 cylindres dans zone rouge
-#clap côté adverse
 
-#poser gobelet 1
-robot.allerA((2600, 600))
 
-#poser gobelet2
-robot.allerA((2600, 1400))
 
-#rentrer
-robot.allerAangle((600, 1000), 0)
-robot.allerA((250, 1000))
+# # poser 2 cylindres dans zone rouge
+# # clap côté adverse
+
+# # poser gobelet 1
+# robot.allerA((2600, 600))
+
+# # poser gobelet2
+# robot.allerA((2600, 1400))
+
+# # rentrer
+# robot.allerAangle((600, 1000), 0)
+# robot.allerA((250, 1000))
 
 
 
